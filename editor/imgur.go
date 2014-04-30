108021af5774408a01ff56ce79afba664a8cec05
Craig Peterson <peterson.craig@gmail.com>
2014-04-29 23:34:10 -0600

    imgur pin code

--- editor/imgur.go
+++ editor/imgur.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"os"
@@ -20,6 +23,17 @@ type ImgurBrowserData struct {
 	Username    string
 }
 
+var AllSessions map[string]*ImgurSession = map[string]*ImgurSession{}
+
+type ImgurCredentials struct {
+	AccessToken     string      `json:"access_token"`
+	ExpiresIn       int         `json:"expires_in"`
+	TokenType       string      `json:"token_type"`
+	Scope           interface{} `json:"scope"`
+	RefreshToken    string      `json:"refresh_token"`
+	AccountUsername string      `json:"account_username"`
+}
+
 var client_id string = os.Getenv("imgur_client_id")
 var client_secret string = os.Getenv("imgur_client_secret")
 var registerUrl string = fmt.Sprintf("https://api.imgur.com/oauth2/authorize?client_id=%s&response_type=pin", client_id)
@@ -29,18 +43,35 @@ func NewBrowserData() *ImgurBrowserData {
 	return &ImgurBrowserData{registerUrl, ""}
 }
 
-func ExchangePinForToken(pin string) error {
+func ExchangePinForToken(pin string) (string, error) {
 	resp, err := http.PostForm(tokenUrl,
 		url.Values{"client_id": {client_id}, "client_secret": {client_secret},
 			"pin": {pin}, "grant_type": {"pin"}})
 	if err != nil {
-		return err
+		return "", err
 	}
 	if resp.StatusCode != 200 {
 		fmt.Println(resp.StatusCode)
-		return nil
+		return "", nil
 	}
-	return nil
+	defer resp.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+	credentials := &ImgurCredentials{}
+	json.Unmarshal(body, credentials)
+	id := generateSessionId()
+	fmt.Println(credentials.AccountUsername)
+	AllSessions[id] = &ImgurSession{credentials.AccountUsername,
+		credentials.AccessToken, credentials.RefreshToken,
+		time.Now().Add(time.Duration(credentials.ExpiresIn) * time.Second)}
+	return id, nil
 }
 
-var AllSessions map[string]*ImgurSession = map[string]*ImgurSession{}
+func generateSessionId() string {
+	alphabet := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var bytes = make([]byte, 20, 20)
+	for i := 0; i < 20; i++ {
+		bytes[i] = alphabet[rand.Intn(len(alphabet))]
+	}
+	return string(bytes)
+}
--- editor/server.go
+++ editor/server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -15,11 +17,35 @@ func main() {
 }
 
 func serveIndex(w http.ResponseWriter, r *http.Request, ren render.Render) {
+	data := NewBrowserData()
+	cookie, _ := r.Cookie("SSID")
+	fmt.Println(cookie.Value)
+	session := AllSessions[cookie.Value]
+	fmt.Println(session)
+	fmt.Println(AllSessions)
 
+	if session != nil {
+		fmt.Println(session.Username)
+		data.Username = session.Username
+	}
 	ren.HTML(200, "editor", NewBrowserData())
 }
 
 func handlePinAuth(w http.ResponseWriter, params martini.Params) {
 	pin := params["pin"]
-	ExchangePinForToken(pin)
+	sessionId, err := ExchangePinForToken(pin)
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		return
+	} else {
+		cookie := &http.Cookie{}
+		cookie.Name = "SSID"
+		cookie.Secure = false
+		cookie.Expires = time.Now().AddDate(5, 0, 0)
+		cookie.Value = sessionId
+		cookie.Domain = "127.0.0.1"
+		cookie.Path = "/"
+		http.SetCookie(w, cookie)
+	}
+
 }
