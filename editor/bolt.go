172d35d43b6fe09254668685d95d76b69686f445
Craig Peterson <cpeterson@ancestry.com>
2015-01-15 10:24:51 -0700

    simplifying db interface for now

--- editor/bolt.go
+++ editor/bolt.go
@@ -4,6 +4,15 @@ import (
 	_ "github.com/boltdb/bolt"
 )
 
+type Image struct {
+	Id   string
+	Name string
+	Data string
+}
+type Database interface {
+	GetExampleImages() ([]*Image, error)
+}
+
 type boltDb struct{}
 
 func NewBoltDb() Database {
@@ -12,24 +21,4 @@ func NewBoltDb() Database {
 
 func (b *boltDb) GetExampleImages() ([]*Image, error) {
 	return nil, nil
-}
-func (b *boltDb) GetUserImages(userId string) ([]*Image, error) {
-	return nil, nil
-}
-func (b *boltDb) SaveUserImage(userId string, img *Image) error {
-	return nil
-}
-func (b *boltDb) CreateUser(username, password, email, anonId string) (string, error) {
-	return "", nil
-}
-func (b *boltDb) CreateAnonUser(anonId string) (string, error) {
-	return "", nil
-}
-
-func (b *boltDb) LookupUser(username, password string) (string, error) {
-	return "", nil
-}
-
-func (b *boltDb) LookupUserFromSessionId(sessionId string) (userId, username string, err error) {
-	return "", "", nil
 }
--- editor/server.go
+++ editor/server.go
@@ -6,21 +6,6 @@ import (
 	"net/http"
 )
 
-type Image struct {
-	Id   string
-	Name string
-	Data string
-}
-type Database interface {
-	GetExampleImages() ([]*Image, error)
-	GetUserImages(userId string) ([]*Image, error)
-	SaveUserImage(userId string, img *Image) error
-	CreateUser(username, password, email, anonId string) (string, error)
-	CreateAnonUser(anonId string) (string, error)
-	LookupUser(username, password string) (string, error)
-	LookupUserFromSessionId(sessionId string) (userId, username string, err error)
-}
-
 func main() {
 	m := martini.Classic()
 	m.Use(render.Renderer(render.Options{Extensions: []string{".tmpl", ".html"}, Delims: render.Delims{"{[{", "}]}"}}))
