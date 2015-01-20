88812de6edb6ffd69f61fe7d956d1936020a64a0
Craig Peterson <cpeterson@ancestry.com>
2015-01-20 11:46:45 -0700

    starting on canvas way.

--- editor/public/app.js
+++ editor/public/app.js
@@ -1,173 +1,51 @@
-angular.module('piet',[])
-
-angular.module('piet')
-	.controller('EditorCtrl', function EditorCtrl($scope,$http) {
-	$scope.palette = makePalette()
-	$scope.program = makeProgram(W,H,DATA) 
-	$scope.settings = {}
-	$scope.editState = {selectedColor:'A',painting:false,rightDown:false,filled:false}
-	$scope.hover = {size:0}
-	console.log($scope.program)
+$(function(){
 	
-	$scope.getCellText = function(cell){
-		return ""
+	var canvas = $("#programCanvas")[0];
+	var programText = DATA;
+	while(programText.length < W*H){
+		programText += "A";
 	}
-	$scope.getCellStyle = function(cell){
-		obj = {"box-sizing":"border-box"}
-		style = "1px solid black"
-		edgeStyle = "2px solid black"
-		if(cell.x == 0){
-			obj["border-left"] = edgeStyle
-		}
-		else if($scope.program.rows[cell.y].cells[cell.x-1].color != cell.color){
-			obj["border-left"] = style
-		}
-		if(cell.x >= ($scope.program.w - 1)){
-			obj["border-right"] = edgeStyle
-		} 
-		else if($scope.program.rows[cell.y].cells[cell.x+1].color != cell.color){
-			obj["border-right"] = style
-		}
-		if(cell.y == 0){
-			obj["border-top"] = edgeStyle
-		}
-		else if( $scope.program.rows[cell.y-1].cells[cell.x].color != cell.color){
-			obj["border-top"] = style
-		}
-		if(cell.y >= ($scope.program.h - 1)){
-			obj["border-bottom"] = edgeStyle
-		} 
-		else if($scope.program.rows[cell.y+1].cells[cell.x].color != cell.color){
-			obj["border-bottom"] = style
-		}
-		return obj
+	var pallette = [];
+	pallette['A'] = "#FFC0C0";
+	pallette['B'] = "#FF0000";
+	pallette['C'] = "#C00000";
+	pallette['D'] = "#FFFFC0";
+	pallette['E'] = "#FFFF00";
+	pallette['F'] = "#C0C000";
+	pallette['G'] = "#C0FFC0";
+	pallette['H'] = "#00FF00";
+	pallette['I'] = "#00C000";
+	pallette['J'] = "#C0FFFF";
+	pallette['K'] = "#00FFFF";
+	pallette['L'] = "#00C0C0";
+	pallette['M'] = "#C0C0FF";
+	pallette['N'] = "#0000FF";
+	pallette['O'] = "#0000C0";
+	pallette['P'] = "#FFC0FF";
+	pallette['Q'] = "#FF00FF";
+	pallette['R'] = "#C000C0";
+	pallette['S'] = "#FFFFFF";
+	pallette['T'] = "#000000";
+	
+	var cellSize = 30;
+	
+	function init(){
+		canvas.width = cellSize * W;
+		canvas.height = cellSize * H;
+		drawAll();
 	}
-	$scope.mouseDown = function(cell,ev){
-		//left click
-		if(ev.which == 1){
-			//right held down. Flood fill.
-			if($scope.editState.rightDown){
-				$scope.editState.filled = true;
-				$scope.hover.size = floodFill(cell,$scope.program,$scope.editState.selectedColor)
-				$scope.hover.size = floodFill(cell,$scope.program)
-			}
-			//regular click. start painting
-			else{
-				cell.color = $scope.editState.selectedColor
-				$scope.editState.painting = true
-				$scope.hover.size = floodFill(cell,$scope.program)
-			}
-		}
-		else if(ev.which == 3){
-			$scope.editState.filled = false;
-			$scope.editState.rightDown = true;
-		}
-	}
-	$scope.mouseEnter = function(cell){
-		if($scope.editState.painting){
-			cell.color = $scope.editState.selectedColor
-		}
-		$scope.hover.size = floodFill(cell,$scope.program)
-	}
-	$scope.mouseUp = function(cell,ev){
-		$scope.editState.painting = false;
-		if(ev.which == 3){
-			$scope.editState.rightDown = false;
-			if (!$scope.editState.filled)
-				$scope.editState.selectedColor = cell.color;
-			$scope.editState.filled = false;
-		}
-	}
-	keys = [110,112,94,43,45,42,47,37,33,62,60,115,100,114,105,73,111,79]
-	$scope.keypress = function(ev){
-		console.log(ev.keyCode, ev.keyIdentifier)
-		idx = keys.indexOf(ev.keyCode)
-		if(idx != -1)
-			$scope.editState.selectedColor = $scope.rotate($scope.editState.selectedColor,Math.floor(idx / 3),idx%3)
-		else if(ev.keyCode == 125){ // } key to cycle colors for whole program
-			rotateColors($scope.program)
-		}
-	}
-	$scope.rotate = function(src,hue,light){
-		x="ABCDEFGHIJKLMNOPQRST"
-		idx = x.indexOf(src)
-		light = ((idx % 3) + light) % 3
-		hue = (Math.floor(idx / 3) + hue) % 6
-		console.log(hue,light)
-		return x[hue*3 + light]
-	}
-	$scope.setColor = function(c){
-		$scope.editState.selectedColor = c
-	}
-});
-
-var mark = 0;
-function floodFill(cell,program,set){
-	mark++
-	var count = 0
-	var stack = [cell]
-	var targetColor = cell.color;
-	while(stack.length){
-		var target = stack.pop()
-		if(target.mark == mark || target.color != targetColor) continue;
-		//not marked, color match
-		target.mark = mark
-		if(set) target.color = set
-		count++
-		if(target.x > 0) stack.push(program.rows[target.y].cells[target.x-1])
-		if(target.x < program.w - 1) stack.push(program.rows[target.y].cells[target.x+1])
-		if(target.y > 0) stack.push(program.rows[target.y-1].cells[target.x])
-		if(target.y < program.h - 1) stack.push(program.rows[target.y+1].cells[target.x])
-	}
-	return count
-}
-
-function rotateColors(program){
-	letters = "ABCDEFGHIJKLMNOPQR"
-	for(var y=0; y<program.h; y++){
-		for(var x = 0; x<program.w; x++){
-			cell = program.rows[y].cells[x]
-			idx = letters.indexOf(cell.color)
-			if(idx != -1){
-				cell.color = letters[(idx+1) % 18]
+	
+	
+	function drawAll(){
+		var ctx = canvas.getContext('2d');
+		for (var y = 0; y< H; y++){
+			for(var x = 0; x < W; x++){
+				var color = programText[y*W + x];
+				ctx.fillStyle = pallette[color];
+				ctx.fillRect(x*cellSize, y*cellSize, cellSize,cellSize)
 			}
 		}
 	}
-}
-
-function makeProgram(w,h,dat){
-	var program = {rows:[],w:w,h:h}
-	for(var y=0; y<h; y++){
-		var row = {cells:[]}
-		for(var x = 0; x<w; x++){
-			c = "S"
-			if(dat) c = dat[x+y*w]
-			row.cells.push({color:c,x:x,y:y,mark:0})
-		}
-		program.rows.push(row)
-	}
-	return program
-}
-
-function makePalette(){
-	return {
-		'A':{color:'A'},
-		'B':{color:'B'},
-		'C':{color:'C'},
-		'D':{color:'D'},
-		'E':{color:'E'},
-		'F':{color:'F'},
-		'G':{color:'G'},
-		'H':{color:'H'},
-		'I':{color:'I'},
-		'J':{color:'J'},
-		'K':{color:'K'},
-		'L':{color:'L'},
-		'M':{color:'M'},
-		'N':{color:'N'},
-		'O':{color:'O'},
-		'P':{color:'P'},
-		'Q':{color:'Q'},
-		'R':{color:'R'},
-	}
-}
+	
+	init();
+})
