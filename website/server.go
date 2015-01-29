a968176eba653feea6d935f40d144b2f181d5f5f
Craig Peterson <cpeterson@ancestry.com>
2015-01-29 16:06:00 -0700

    gifs ready to cause seizures!

--- website/server.go
+++ website/server.go
@@ -28,7 +28,8 @@ func main() {
 	m.Get("/examples", serveExamples)
 	m.Post("/save", saveImg)
 	m.Get("/:id", serveImg)
-	m.Get("/img/(?P<id>~?[a-zA-Z0-9]+).png", renderImage)
+	m.Get("/img/(?P<id>~?[a-zA-Z0-9]+).png", renderPng)
+	m.Get("/img/(?P<id>~?[a-zA-Z0-9]+).gif", renderGif)
 	m.Run()
 }
 
@@ -68,8 +69,13 @@ func serveImg(w http.ResponseWriter, params martini.Params, ren render.Render) {
 func serveExamples(ren render.Render) {
 	ren.HTML(200, "examples", database.GetExampleImages())
 }
-
-func renderImage(w http.ResponseWriter, r *http.Request, params martini.Params) {
+func renderGif(w http.ResponseWriter, r *http.Request, params martini.Params) {
+	renderImage(w, r, params, "G")
+}
+func renderPng(w http.ResponseWriter, r *http.Request, params martini.Params) {
+	renderImage(w, r, params, "P")
+}
+func renderImage(w http.ResponseWriter, r *http.Request, params martini.Params, format string) {
 	id := params["id"]
 	img, err := database.GetImage(id)
 	if err != nil {
@@ -80,15 +86,21 @@ func renderImage(w http.ResponseWriter, r *http.Request, params martini.Params) 
 
 	cs := getQuery(r.URL, "cs", 1, 80, 10)
 	rotate := getQuery(r.URL, "rot", 0, 17, 0)
-	w.Header().Add("Content-Type", "image/png")
-	cacheKey := fmt.Sprintf("%s-%d-%d", id, cs, rotate)
+
+	cacheKey := fmt.Sprintf("%s-%d-%d-%s", id, cs, rotate, format)
 	data, ok := cache.Get(cacheKey)
 	var b []byte
 	if ok {
 		b = data.([]byte)
 	} else {
 		buf := bytes.NewBuffer(nil)
-		images.BuildImage(img.Width, img.Height, img.Data, cs, rotate, buf)
+		if format == "G" {
+			w.Header().Add("Content-Type", "image/gif")
+			images.BuildGif(img.Width, img.Height, img.Data, cs, buf)
+		} else {
+			w.Header().Add("Content-Type", "image/png")
+			images.BuildImage(img.Width, img.Height, img.Data, cs, rotate, buf)
+		}
 		b = buf.Bytes()
 		cache.Add(cacheKey, b)
 	}
