cd7ec5def1d0f72cea5948bbbf85d595cbcc8dc6
Craig Peterson <cpeterson@ancestry.com>
2015-01-29 16:01:52 -0700

    image rotation implemented. Gifs ready, ut not exposed

--- images/imageBuilder.go
+++ images/imageBuilder.go
@@ -1,133 +1,38 @@
 package images
 
 import (
-	"bytes"
-	"compress/zlib"
-	"encoding/binary"
-	"fmt"
-	"golang.org/x/image/bmp"
-	"hash/crc32"
 	"image"
+	"image/gif"
 	"image/png"
 	"io"
 )
 
-func BuildImage(w, h int, data string, codelSize int, writer io.Writer) {
-	img := image.NewPaletted(image.Rect(0, 0, w*codelSize, h*codelSize), palette)
-	for y := 0; y < h; y++ {
-		for x := 0; x < w; x++ {
-			color := data[y*w+x]
-			idx := color - 'A'
-			startX := x * codelSize
-			startY := y * codelSize
-			endX := x*codelSize + codelSize
-			endY := y*codelSize + codelSize
-			for xx := startX; xx <= endX; xx++ {
-				for yy := startY; yy < endY; yy++ {
-					img.SetColorIndex(xx, yy, idx)
-				}
-			}
-		}
-	}
+func BuildImage(w, h int, data string, codelSize, rotation int, writer io.Writer) {
+	img := makeImage(w, h, data, codelSize, rotation)
 	png.Encode(writer, img)
 }
 
-func BuildImageRaw(w, h int, data string, codelSize int, writer io.Writer) {
-	hdr := []byte{137, 80, 78, 71, 13, 10, 26, 10}
-	writer.Write(hdr)
-	buf := new(bytes.Buffer)
-	binary.Write(buf, binary.BigEndian, int32(w*codelSize))
-	binary.Write(buf, binary.BigEndian, int32(h*codelSize))
-	buf.WriteByte(8)
-	buf.WriteByte(3)
-	buf.WriteByte(0)
-	buf.WriteByte(0)
-	buf.WriteByte(0)
-	writeChunk("IHDR", buf.Bytes(), writer)
-	writeChunk("PLTE", []byte{255, 192, 192, 255, 0, 0, 192, 0, 0, 255, 255, 192, 255, 255, 0, 192, 192, 0, 192, 255, 192, 0, 255, 0, 0, 192, 0, 192, 255, 255, 0, 255, 255, 0, 192, 192, 192, 192, 255, 0, 0, 255, 0, 0, 192, 255, 192, 255, 255, 0, 255, 192, 0, 192, 255, 255, 255, 0, 0, 0}, writer)
-	buf = new(bytes.Buffer)
-	z, _ := zlib.NewWriterLevel(buf, zlib.BestCompression)
-
-	row := make([]byte, w*codelSize+1)
-	noopRow := make([]byte, w*codelSize+1)
-	noopRow[0] = 2
-	row[0] = 1
-	for y := 0; y < h; y++ {
-		lastColor := uint8(0)
-		for x := 0; x < w; x++ {
-			color := data[y*w+x]
-			idx := color - 'A'
-			row[(x*codelSize)+1] = idx - lastColor
-			lastColor = idx
-
-		}
-		z.Write(row)
-		for i := 1; i < codelSize; i++ {
-			z.Write(noopRow)
-		}
+func BuildGif(w, h int, data string, codelSize int, writer io.Writer) {
+	g := gif.GIF{}
+	g.Image = make([]*image.Paletted, 18)
+	g.Delay = make([]int, 18)
+	g.LoopCount = -1
+	for i := 0; i < 18; i++ {
+		g.Image[i] = makeImage(w, h, data, codelSize, i)
+		g.Delay[i] = 12
 	}
-	z.Flush()
-	writeChunk("IDAT", buf.Bytes(), writer)
-	writeChunk("IEND", []byte{}, writer)
+	gif.EncodeAll(writer, &g)
 }
 
-func writeChunk(typ string, data []byte, w io.Writer) {
-	binary.Write(w, binary.BigEndian, int32(len(data)))
-	w.Write([]byte(typ))
-	w.Write(data)
-	hash := crc32.NewIEEE()
-	hash.Write([]byte(typ))
-	hash.Write(data)
-	binary.Write(w, binary.BigEndian, hash.Sum32())
-}
-
-func BuildImageBmpRaw(w, h int, data string, codelSize int, writer io.Writer) {
-	//using rle: size of data potion = rows * (cols * 2) + 2?
-	rowSize := w * codelSize * 1
-	if rowSize%4 != 0 {
-		rowSize += 4 - (rowSize % 4)
-	}
-	imgSize := h * codelSize * rowSize
-	palletSize := 20 * 4
-	fileSize := imgSize + 54 + palletSize
-	writer.Write([]byte("BM"))
-	binary.Write(writer, binary.LittleEndian, int32(fileSize))
-	//reserved
-	writer.Write([]byte{0, 0, 0, 0})
-	//offset
-	binary.Write(writer, binary.LittleEndian, int32(54+palletSize)) //+pallet size
-	//size of header
-	writer.Write([]byte{0x28, 0, 0, 0})
-	//width
-	binary.Write(writer, binary.LittleEndian, int32(w*codelSize))
-	//height
-	binary.Write(writer, binary.LittleEndian, int32(h*codelSize))
-	//color planes / bpp/ compression
-	writer.Write([]byte{1, 0, 24, 0, 0, 0, 0, 0})
-	//bitmap data size
-	binary.Write(writer, binary.LittleEndian, int32(imgSize))
-	//resolution / resolution / pallette size / important colors
-	writer.Write([]byte{0x13, 0xb, 0, 0, 0x13, 0xb, 0, 0 /*pallete size*/, 20, 0, 0, 0, 0, 0, 0, 0})
-	//pallette
-	writer.Write([]byte{255, 192, 192, 0, 255, 0, 0, 0, 192, 0, 0, 0, 255, 255, 192, 0, 255, 255, 0, 0, 192, 192, 0, 0, 192, 255, 192, 0, 0, 255, 0, 0, 0, 192, 0, 0, 192, 255, 255, 0, 0, 255, 255, 0, 0, 192, 192, 0, 192, 192, 255, 0, 0, 0, 255, 0, 0, 0, 192, 0, 255, 192, 255, 0, 255, 0, 255, 0, 192, 0, 192, 0, 255, 255, 255, 0, 0, 0, 0, 0})
-	row := make([]byte, rowSize)
-	for i := 0; i < h; i++ {
-		for j := 0; j < w; j++ {
-			color := data[i*w+j]
-			idx := color - 'A'
-			c := colors[idx]
-			fmt.Println(c.RGBA())
-		}
-		writer.Write(row)
-	}
-}
-
-func BuildImageBmp(w, h int, data string, codelSize int, writer io.Writer) {
+func makeImage(w, h int, data string, codelSize, rotation int) *image.Paletted {
+	hueAdd := uint8(rotation / 3)
+	ligAdd := uint8(rotation % 3)
 	img := image.NewPaletted(image.Rect(0, 0, w*codelSize, h*codelSize), palette)
 	for y := 0; y < h; y++ {
 		for x := 0; x < w; x++ {
 			color := data[y*w+x]
 			idx := color - 'A'
+			idx = rotateColor(idx, hueAdd, ligAdd)
 			startX := x * codelSize
 			startY := y * codelSize
 			endX := x*codelSize + codelSize
@@ -139,5 +44,14 @@ func BuildImageBmp(w, h int, data string, codelSize int, writer io.Writer) {
 			}
 		}
 	}
-	bmp.Encode(writer, img)
+	return img
+}
+
+func rotateColor(idx, hueAdd, ligAdd uint8) uint8 {
+	if idx > 17 {
+		return idx
+	}
+	hue := ((idx / 3) + hueAdd) % 6
+	lig := ((idx % 3) + ligAdd) % 3
+	return (hue * 3) + lig
 }
--- website/server.go
+++ website/server.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/golang-lru"
 	"github.com/martini-contrib/render"
 	"net/http"
+	"net/url"
 	"strconv"
 )
 
@@ -70,40 +71,43 @@ func serveExamples(ren render.Render) {
 
 func renderImage(w http.ResponseWriter, r *http.Request, params martini.Params) {
 	id := params["id"]
-	cs := 10
-	csQuery := r.URL.Query().Get("cs")
-	if csQuery != "" {
-		cs64, err := strconv.ParseInt(csQuery, 10, 32)
-		if err == nil {
-			cs = int(cs64)
-			if cs > 80 {
-				cs = 80
-			}
-			if cs < 1 {
-				cs = 1
-			}
-		}
-	}
-	fmt.Println(id)
 	img, err := database.GetImage(id)
 	if err != nil {
 		w.WriteHeader(404)
 		fmt.Println(err)
 		return
 	}
+
+	cs := getQuery(r.URL, "cs", 1, 80, 10)
+	rotate := getQuery(r.URL, "rot", 0, 17, 0)
 	w.Header().Add("Content-Type", "image/png")
-	cacheKey := fmt.Sprintf("%s-%d", id, cs)
-	fmt.Println(cacheKey)
+	cacheKey := fmt.Sprintf("%s-%d-%d", id, cs, rotate)
 	data, ok := cache.Get(cacheKey)
-	fmt.Println(data, ok)
 	var b []byte
 	if ok {
 		b = data.([]byte)
 	} else {
 		buf := bytes.NewBuffer(nil)
-		images.BuildImage(img.Width, img.Height, img.Data, cs, buf)
+		images.BuildImage(img.Width, img.Height, img.Data, cs, rotate, buf)
 		b = buf.Bytes()
 		cache.Add(cacheKey, b)
 	}
 	w.Write(b)
+}
+
+func getQuery(u *url.URL, name string, min, max, def int) int {
+	if q := u.Query().Get(name); q != "" {
+		v64, err := strconv.ParseInt(q, 10, 32)
+		if err == nil {
+			v := int(v64)
+			if v > max {
+				v = max
+			}
+			if v < min {
+				v = min
+			}
+			return v
+		}
+	}
+	return def
 }
